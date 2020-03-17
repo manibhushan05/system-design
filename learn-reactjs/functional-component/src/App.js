d22427b0646f2febae65d20c97f30bbfc6ea37eb
Mani Kumar <manikumar@ManiBhushan.local>
2020-03-17 20:34:54 +0530

    initial release

---
+++ learn-reactjs/functional-component/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {Greet} from './components/Greet'
+
+function App() {
+  return (
+    <div className="App">
+      <Greet/>
+    </div>
+  );
+}
+
+export default App;
