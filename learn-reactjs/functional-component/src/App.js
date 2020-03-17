c3d8ab0338c1de880f4d95af087602212bec047f
Mani Kumar <manikumar@ManiBhushan.local>
2020-03-17 20:51:31 +0530

    changed

--- learn-reactjs/functional-component/src/App.js
+++ learn-reactjs/functional-component/src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import {Greet} from './components/Greet'
+import Welcome from './components/Welcome'
 
-function App() {
+const App = ()=>{
   return (
     <div className="App">
       <Greet/>
+      <Welcome/>
     </div>
-  );
+  )
 }
 
 export default App;
